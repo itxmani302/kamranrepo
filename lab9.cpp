29c5dc4fb2cae19ef03479df9c0ddcc70bc3e880
itxmani302 <154399617+itxmani302@users.noreply.github.com>
2023-12-20 09:23:52 +0500

    Add files via upload

---
+++ lab9.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+class libary{
+	private:
+	string title;
+	string auther;
+  int pyear;
+   int isbn;
+	public:
+	library(int pye,string auth, string tit){
+	
+page=pg;
+auther=aut;
+title=t
+}
+	                                                                                                                                                                                   
+
+void addbook(string title, string author){
+	cout<<"enter the auther"<<auther<<endl;
+	cout<<"enter the title"<<title<<endl;
+	
+void addbook(string  auth,string title,string isbn ) {
+	cout<<"enter the auther"<<auther<<endl;
+	cout<<"entert a title"<<title<<endl;
+	cout<<"isbn"<<isbn<<endl;
+
+}
+void addbook(string title, string author,int isbn){
+	cout<<"enter the tittle"<<title<<endl;
+	cout<<"enter the auther"<<author<<endl;
+	cout<<"enter the isbn <<isbn<<endl"endl;
+	
+};
+
+int main ()
+{
+	labaray obj;
+	obj.addbooks("hasham""hasham");
+
+}
